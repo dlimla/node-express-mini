f2f8400c12c8cf29ed9f409c3689a9effc20e3b2
Daniel Lim <danheelim@gmail.com>
2019-02-27 18:47:12 -0800

    sets up server and gets all users

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // implement your API here
+
+const express = require('express');
+
+const server = express();
+
+const port = '9000';
+
+const db = require('./data/db.js')
+
+server.get('/',(req,res) => {
+    res.send("Hello World")
+})
+
+server.get('/api/users', (req,res) => {
+    db.find()
+    .then(user => {
+        res.json(user);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+})
+
+
+server.listen(port, () => {
+    console.log(`Server is listening in port ${port}`)
+})
